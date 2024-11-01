55afd281528e6eb2f2c40b13f3c9ae91425892d5
khimxzxc <127808298+khimxzxc@users.noreply.github.com>
2024-11-01 15:09:33 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,40 +1,20 @@
-function isPrime(num) {
-    if (num <= 1) return false;
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i === 0) return false;
-    }
-    return true;
-}
+let calculator = '+'
 
-function calculateSum() {
-    const num1 = parseInt(document.getElementById('number1').value);
-    const num2 = parseInt(document.getElementById('number2').value);
+document.getElementById('plus').onclick = () => calculator = '+'
+document.getElementById('minus').onclick = () => calculator = '-'
+document.getElementById('multiplication').onclick = () => calculator = '*'
+document.getElementById('division').onclick = () => calculator = '/'
+
+document.getElementById('submit').onclick = () => {
+    const num1 = Number(input1.value)
+    const num2 = Number(input2.value)
     
-    if (!isPrime(num1) || !isPrime(num2)) {
-        document.getElementById('result').innerHTML = 
-            'Ошибка: оба числа должны быть простыми!';
-        return;
+    const operations = {
+        '+': (num1, num2) => num1 + num2,
+        '-': (num1, num2) => num1 - num2,
+        '*': (num1,num2) => num1 * num2,
+        '/': (num1, num2) => num1 / num2
     }
-
-    const sum = num1 + num2;
     
-    document.getElementById('result').innerHTML = 
-        `Результат: ${num1} + ${num2} = ${sum}`;
+    document.getElementById('result').textContent = operations[calculator](num1, num2)
 }
-
-//функция для сброса калькулятора
-function resetCalculator() {
-    // Очищаем поля ввода
-    document.getElementById('number1').value = '';
-    document.getElementById('number2').value = '';
-    // Сбрасываем результат
-    document.getElementById('result').innerHTML = 'Результат: ';
-}
-
-document.getElementById('number1').addEventListener('input', function() {
-    if (this.value < 0) this.value = 0;
-});
-
-document.getElementById('number2').addEventListener('input', function() {
-    if (this.value < 0) this.value = 0;
-});
